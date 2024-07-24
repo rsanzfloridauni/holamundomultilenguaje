f33c03025764aba7014cc5be7a6684820d671cba
rsanzfloridauni <rsanz@florida-uni.es>
2024-07-24 18:11:38 +0200

    Modificación en index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Saludo Aleatorio</title>
+    <title>Saludo aleatorio</title>
     <style>
         body {
             font-family: Arial, sans-serif;
